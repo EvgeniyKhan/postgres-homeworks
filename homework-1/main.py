9a741d5139664bc8f1ff60636d37a000e3345dd9
evgeniy <evgeniy.igorevich9402@gmail.com>
2024-01-12 10:05:06 +0900

    add func insert_data

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,28 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+
+FILE_CUSTOMERS_DATA = "north_data/customers_data.csv"
+FILE_EMPLOYEES_DATA = "north_data/employees_data.csv"
+FILE_ORDERS_DATA = "north_data/orders_data.csv"
+
+
+def insert_data_to_sql(filename, tablename):
+    try:
+        with open(filename, "r", encoding="UTF-8") as file:
+            data = csv.DictReader(file)
+            with psycopg2.connect(host='localhost', database='north', user='postgres', password='Igorevi4') as conn:
+                with conn.cursor() as cursor:
+                    for row in data:
+                        row_data = []
+                        values_designation = []
+                        for key in list(row.keys()):
+                            row_data.append(row[key])
+                            values_designation.append("%s")
+                        cursor.execute(f"INSERT INFORMATION {tablename} VALUES ({', '.join(values_designation)})",
+                                       tuple(row_data))
+                        row_data.clear()
+                        values_designation.clear()
+    finally:
+        conn.close()
